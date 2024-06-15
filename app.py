377100be7b8d2b24d4842712dc566183777006b2
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-15 22:35:47 +0530

    Integrate PDF upload handling and generative AI responses for resume evaluation against job descriptions with HR and ATS perspectives

--- app.py
+++ app.py
@@ -60,6 +60,37 @@ if uploaded_file is not None:
 
 submit1 = st.button("Tell Me About the Resume")
 
-submit2 = st.button("How Can I Improvise my Skills")
+# submit2 = st.button("How Can I Improvise my Skills")
 
 submit3 = st.button("Percentage match")
+
+input_prompt1 = """
+ You are an experienced HR With Tech Experience in the field of any one job role from Data Science or Full stack , Web Development , Big Data Enginnering, Software Enginnering Intern, DEVOPS,Data Analyst, CyberSecurity your task is to review the provided resume against the job description for these profiles. 
+  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+ Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+"""
+
+
+input_prompt3 = """
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of any one job role Data Science,Full stack, Web Development , Big Data Enginnering, Software Enginnering Intern, DEVOPS,Data Analyst, CyberSecurity and ATS functionality, 
+your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Response is")
+        st.write(response)
+    else:
+        st.write("Please Upload a pdf File")
+
+elif submit3:
+     if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt3,pdf_content,input_text)
+        st.subheader("The Response is")
+        st.write(response)
+     else:
+        st.write("Please Upload a pdf File")
