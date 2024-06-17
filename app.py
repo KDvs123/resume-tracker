515b4d786935c5021aa20c7a74f042e1ac74e00f
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-17 18:21:20 +0530

    Add functions to read text from PDF files and display PDFs in Streamlit.

--- app.py
+++ app.py
@@ -32,3 +32,29 @@ csv = df.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode() # some strings <-> bytes conversions necessary here
 # href = f'<a href="data:file/csv;base64,{b64}">Download Report</a>'
 href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+    
